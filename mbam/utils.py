ccfd75069514f0ab616c5716a32be7a7adf6f0dc
yonatank <kurniawanyo@outlook.com>
2022-07-06 16:15:23 -0600

    ENH: Added options to specify index and forward in initial_velocity

--- mbam/utils.py
+++ mbam/utils.py
@@ -5,9 +5,8 @@
 import numpy as np
 
 
-def initial_velocity(x, jac, Avv):
-    """A routine for calculating the initial velocity in the sloppiest
-    direction.
+def initial_velocity(x, jac, Avv, i=0, forward=True):
+    """A routine for calculating the initial velocity.
 
     Parameters
     ----------
@@ -17,6 +16,13 @@ def initial_velocity(x, jac, Avv):
         Function for calculating the jacobian.
     Avv: callable ``Avv(x, v)``
         Function for calculating a direction second derivative.
+    i: int (optional)
+        Index to choose which eigenvector direction to choose: 0 = sloppiest,
+        1 = second sloppiest, etc.
+    forward: bool (optional)
+        A flag where if it is set to ``True``, the direction in which the
+        velocity increases around ``x`` is chosen. Otherwise, the opposite
+        direction is chosen.
 
     Returns
     -------
@@ -26,10 +32,14 @@ def initial_velocity(x, jac, Avv):
 
     j = jac(x)
     _, _, vh = np.linalg.svd(j)
-    v = vh[-1]
+    v = vh[-1 - i]
     a = -np.linalg.solve(j.T @ j, j.T @ Avv(x, v))
     # We choose the direction in which the velocity will increase, since this
     # is most likely to find a singularity quickest.
     if v @ a < 0:
         v *= -1
-    return v
+
+    if forward:
+        return v / np.linalg.norm(v)
+    else:
+        return -v / np.linalg.norm(v)
---
+++ tests/test_initial_velocity.py
@@ -0,0 +1,26 @@
+import numpy as np
+import mbam.finite_difference as fd
+from mbam.utils import initial_velocity
+
+from model_linear import M, N, f, J, xi
+
+
+_, _, vh = np.linalg.svd(J)
+jac_fn = fd.jacobian_func(f, M, N)
+Avv_fn = fd.Avv_func(f)
+
+
+def test_initial_velocity():
+    """We test the utility function to get initial velocity using a linear
+    model. Since Avv for a linear model is zero, then the initial velocities
+    should be the same as the right singular vectors of the Jacobian.
+    """
+    for i in range(N):
+        for forward in [True, False]:
+            v = initial_velocity(xi, jac_fn, Avv_fn, i=i, forward=forward)
+            v_ref = vh[-1 - i] * (int(forward) * 2 - 1)
+            assert np.allclose(v, v_ref)
+
+
+if __name__ == "__main__":
+    test_initial_velocity()
